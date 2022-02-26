2b6399fcd199db98f088b2fe14aebe0767964360
EzgiTasdelen <100235474+EzgiTasdelen@users.noreply.github.com>
2022-02-27 02:56:40 +0300

    Add files via upload

---
+++ 8_ekstra_ornek_/8_ekstra_ornek_/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _8_ekstra_ornek_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            islem devlet1 = new islem();
+            devlet1.Ad = "Türkiye";
+            devlet1.Baskent = "Ankara";
+            devlet1.ParaBirimi = "TL";
+            devlet1.Nufus = 800000;
+            devlet1.Yazdir();
+
+            islem devlet2 = new islem();
+            devlet2.Ad = "Almanya";
+            devlet2.Baskent = "Berlin";
+            devlet2.ParaBirimi = "Euro";
+            devlet2.Nufus = 745000;
+            devlet2.Yazdir();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 8_ekstra_ornek_/8_ekstra_ornek_/islem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _8_ekstra_ornek_
+{
+    class islem
+    {
+        string ad;
+
+        public string Ad
+        {
+           get { return ad; }
+            set { ad = value; }
+        }
+        string baskent;
+
+        public string Baskent
+        {
+            get { return baskent; }
+            set { baskent = value; }
+        }
+
+        int nufus = 1500;
+
+        public int Nufus
+        {
+            get { return nufus; }
+            set { nufus = value; }
+        }
+
+        string paraBirimi;
+
+        public string ParaBirimi
+        {
+            get { return paraBirimi; }
+            set { paraBirimi = value; }
+        }
+
+        public void Yazdir()
+        {
+            Console.WriteLine("Ülke Adı      :" + ad);
+
+            Console.WriteLine("Başkenti      :" + baskent);
+
+            Console.WriteLine("Nüfusu        :" + nufus);
+
+            Console.WriteLine("Para Birimi   :" + paraBirimi);
+
+            Console.WriteLine("==========================================");
+
+
+        }
+    }
+}
